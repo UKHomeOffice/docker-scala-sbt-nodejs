95b6e8ba055611535b6acb03e34648e25b2b5a07
Rich Birch <ringo@albumsnaps.com>
2025-01-21 08:42:20 +0000

    DRTII-1692 Fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Set ACP Artefactory as the proxy for dependency resolution
+# Set ACP Artifactory as the proxy for dependency resolution
 echo -en "realm=Artifactory Realm\nhost=artifactory.digital.homeoffice.gov.uk\nuser=$ARTIFACTORY_USERNAME\npassword=$ARTIFACTORY_PASSWORD" > /app/.ivy2/.credentials
 export SBT_CREDENTIALS="/app/.ivy2/.credentials"
 export SBT_OPTS="-Duser.home=/app -Dsbt.override.build.repos=true -Dsbt.ivy.home=/app/.ivy2"
