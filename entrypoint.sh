74031a426f909afd99dfd2280c32e117b462d584
Rich Birch <ringo@albumsnaps.com>
2025-01-21 11:46:04 +0000

    DRTII-1692 mkdir .ivy2 before writing a file to it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Set ACP Artifactory as the proxy for dependency resolution
+mkdir -p /app/.ivy2
 echo -en "realm=Artifactory Realm\nhost=artifactory.digital.homeoffice.gov.uk\nuser=$ARTIFACTORY_USERNAME\npassword=$ARTIFACTORY_PASSWORD" > /app/.ivy2/.credentials
 export SBT_CREDENTIALS="/app/.ivy2/.credentials"
 export SBT_OPTS="-Duser.home=/app -Dsbt.override.build.repos=true -Dsbt.ivy.home=/app/.ivy2"
